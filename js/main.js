7ca1256b98508040e06e844622ebd61b48d3a830
MehrdadAdabi <mehrdadadabi29@gmail.com>
2023-03-09 10:39:55 +0330

    add pages in index page

--- js/main.js
+++ js/main.js
@@ -1,191 +1,261 @@
 const SIDE = {
-    MAFIA: "mafia",
-    CITY: "city",
+  MAFIA: "mafia",
+  CITY: "city",
 };
 
 const STATE = {
-    LIVE: "live",
-    DIE: "die",
-    Election: "election",
-    KICK: "kick"
+  LIVE: "live",
+  DIE: "die",
+  Election: "election",
+  KICK: "kick",
 };
 
 const IN_GAME = {
-    ACTIVE: true,
-    DEACTIVE: false
-}
+  ACTIVE: true,
+  DEACTIVE: false,
+};
 
 const PLAYERS = "players";
 const CHARACTERS = "characters";
 
 $(() => {
-    clicks.forEach(i => on.click(i.selector, i.func));
+  clicks.forEach((i) => on.click(i.selector, i.func));
 
-    let list = storage.run(PLAYERS);
-    if (list && list.length > 0) {
-        Player.List = list.fromJson();
-        Player.List.forEach(i => dom.addPlayer(i));
-    }
+  let list = storage.run(PLAYERS);
+  if (list && list.length > 0) {
+    Player.List = list.fromJson();
+    Player.List.forEach((i) => dom.addPlayer(i));
+  }
 });
 
 class Player {
-    constructor(name) {
-        this.id = getId();
-        this.characterId = 0;
-        this.name = name;
-        this.inGame = IN_GAME.ACTIVE;
-        this.state = STATE.LIVE;
-    }
-    static List = [];
+  constructor(name) {
+    this.id = getId();
+    this.characterId = 0;
+    this.name = name;
+    this.inGame = IN_GAME.ACTIVE;
+    this.state = STATE.LIVE;
+  }
+  static List = [];
 
-    static Get(id) { return this.List.find(i => i.id == id) };
+  static Get(id) {
+    return this.List.find((i) => i.id == id);
+  }
 
-    static Add(player) {
-        this.List.push(player);
-        storage.run(PLAYERS, this.List.toJson());
-        $('players').html("")
-        this.List.forEach(i => dom.addPlayer(i));
-    }
+  static Add(player) {
+    this.List.push(player);
+    storage.run(PLAYERS, this.List.toJson());
+    $("players").html("");
+    this.List.forEach((i) => dom.addPlayer(i));
+  }
 
-    static Delete(id) {
-        let found = this.Get(id);
-        if (found) {
-            this.List.remove(found);
-            storage.run(PLAYERS, this.List.toJson())
-        }
+  static Delete(id) {
+    let found = this.Get(id);
+    if (found) {
+      this.List.remove(found);
+      storage.run(PLAYERS, this.List.toJson());
     }
+  }
 
-    static Edit() {
-        storage.run(PLAYERS, this.List.toJson())
-    }
+  static Edit() {
+    storage.run(PLAYERS, this.List.toJson());
+  }
 }
 
 class Character {
-    constructor(id, name, description, counterAbility, nickname, side) {
-        this.id = id || 0;
-        this.name = name || "";
-        this.description = description || "";
-        this.counterAbility = counterAbility || 0;
-        this.nickname = nickname || "";
-        this.side = side || null;
-    }
+  constructor(id, name, description, counterAbility, nickname, side) {
+    this.id = id || 0;
+    this.name = name || "";
+    this.description = description || "";
+    this.counterAbility = counterAbility || 0;
+    this.nickname = nickname || "";
+    this.side = side || null;
+  }
 }
 
 var characters = [];
 
 const clicks = [
-    {
-        selector: '.addPlayer .btn',
-        func() {
-            let p = new Player();
-            let name = $('.txtName').val();
-            if (name.length >= 2) {
-                p.name = name;
-                Player.Add(p);
-            } else {
-                // showMessage("خطا", "لطفا نام کاربر را وارد نمایید.", "error");
-                // return false;
-            }
-        }
+  {
+    selector: ".addPlayer .btn",
+    func() {
+      let p = new Player();
+      let name = $(".txtName").val();
+      if (name.length >= 2) {
+        p.name = name;
+        Player.Add(p);
+      } else {
+        message.show(
+          "خطا",
+          ".نام کاربر وارد شده کمتر از حد مجاز می باشد",
+          "error"
+        );
+        $("backWard").addClass("active");
+        return false;
+      }
     },
-    {
-        selector: '.removePlayer',
-        func(e) {
-            let id = e.closest(".playerCondition").attr("id");
-            Player.Delete(id);
-            dom.removePlayer(e);
-        }
+  },
+  {
+    selector: ".removePlayer",
+    func(e) {
+      let id = e.closest(".playerCondition").attr("id");
+      Player.Delete(id);
+      dom.removePlayer(e);
     },
-    {
-        selector: '.playerCondition .checkbox input',
-        func(e) {
-            let id = e.closest(".playerCondition").attr("id");
-            let inGame = e.is(':checked');
-            let found = Player.Get(id);
-            found.inGame = inGame;
-            Player.Edit(found);
-        }
-    }
+  },
+  {
+    selector: ".playerCondition .checkbox input",
+    func(e) {
+      let id = e.closest(".playerCondition").attr("id");
+      let inGame = e.is(":checked");
+      let found = Player.Get(id);
+      found.inGame = inGame;
+      Player.Edit(found);
+    },
+  },
+
+  {
+    selector: "backWard , .closeBtn",
+    func(e) {
+      $("backward").removeClass("active");
+      $(".alertBox").addClass("hide");
+    },
+  },
+
+  {
+    selector: ".next",
+    func(e) {
+      nextPage(e);
+    },
+  },
+  {
+    selector: ".prev",
+    func(e) {
+      prevPage(e);
+    },
+  },
 ];
 
-
-
-
 var dom = {
-    addPlayer: player => {
-        let res = `<div id="${player.id}" class="playerCondition">
+  addPlayer: (player) => {
+    let res = `<div id="${player.id}" class="playerCondition">
                     <a href="#" class="ligthShadow btn ico danger removePlayer">
                         <icon class="disAgree"></icon>
                     </a>
                     <div class="inputBox ligthShadow left">
-                        <input type="text" placeholder="نام بازکن" value="${player.name}" disabled>
+                        <input type="text" placeholder="نام بازکن" value="${
+                          player.name
+                        }" disabled>
                     </div>
-                    <label for="chk#${player.id}" class="checkbox ligthShadow rigth">
-                        <input id="chk#${player.id}" type="checkbox" class="hide" ${player.inGame ? "checked" : "" }>
+                    <label for="chk#${
+                      player.id
+                    }" class="checkbox ligthShadow rigth">
+                        <input id="chk#${
+                          player.id
+                        }" type="checkbox" class="hide" ${
+      player.inGame ? "checked" : ""
+    }>
                         <span class="checkmark"></span>
                     </label>
                 </div>`;
-        $('.txtName').val("");
-        $('players').append(res);
-    },
-    removePlayer: e => e.parent('.playerCondition').remove()
-}
+    $(".txtName").val("");
+    $("players").append(res);
+  },
+  removePlayer: (e) => e.parent(".playerCondition").remove(),
+};
 
 const message = {
-    show(title, text, bg) {
-        let alert = `
+  show(title, text, bg) {
+    let alert = `
             <div msg=${bg} class='alertBox'>
+            <a class="ico closeBtn">
+            <icon class='close'></icon>
+            </a>
                 <title>${title}</title>
                 <p class='txtBox'>${text}</p>
             </div>`;
-        $("body").prepend(alert);
-    }
-}
+    $("body").prepend(alert);
+  },
+};
 
 var on = {
-    click: (selector, func) => $("body").delegate(selector, "click", function (e) {
-        if (func) func($(this));
-    })
-}
-
+  click: (selector, func) =>
+    $("body").delegate(selector, "click", function (e) {
+      if (func) func($(this));
+    }),
+};
 
 const storage = {
-    run(key, value) {
-        if (value != null)
-            localStorage.setItem(key, value);
-        return localStorage.getItem(key);
-    },
-    remove: key => localStorage.removeItem(key)
-}
+  run(key, value) {
+    if (value != null) localStorage.setItem(key, value);
+    return localStorage.getItem(key);
+  },
+  remove: (key) => localStorage.removeItem(key),
+};
 function getId() {
-
-    var S4 = function () {
-        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-    };
-    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+  var S4 = function () {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+  return (
+    S4() +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    S4() +
+    S4()
+  );
 }
 
 function defineMethod(name, func) {
-    Object.defineProperty(Object.prototype, name, {
-        value: func,
-        writable: true,
-        configurable: true
-    });
+  Object.defineProperty(Object.prototype, name, {
+    value: func,
+    writable: true,
+    configurable: true,
+  });
 }
 
 defineMethod("toJson", function () {
-    return JSON.stringify(this);
+  return JSON.stringify(this);
 });
 
 defineMethod("fromJson", function () {
-    return JSON.parse(this);
-})
-
-defineMethod("remove", function (i) {
-    const index = this.indexOf(i);
-    if (index > -1) {
-        this.splice(index, 1);
-    }
+  return JSON.parse(this);
 });
 
+defineMethod("remove", function (i) {
+  const index = this.indexOf(i);
+  if (index > -1) {
+    this.splice(index, 1);
+  }
+});
 
+const nextPage = (btn) => {
+  let page = btn.closest("page");
+  let pnlBtn = btn.parent().addClass("hide");
+  let next = page.next().attr("active", true);
+  let prev = page.removeAttr("active");
+};
+
+const prevPage = (btn) => {
+  let page = btn.closest("page");
+  if (btn.hasClass("cancle")) {
+    message.show("هشدار", "آیا می خواهید از بازی خارج شوید؟", "pending");
+    $(".modal").addClass("active");
+    return false;
+  } else {
+    let pnlBtn = btn
+      .closest("page")
+      .prev()
+      .find(".panelEvent")
+      .removeClass("hide");
+    let item = page.removeAttr("active");
+    let prev = page.prev().attr("active", true);
+    let next = page.next().removeAttr("active");
+  }
+};
